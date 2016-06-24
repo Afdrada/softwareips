a102bfe952c5d2dc5448d6f4de0109517a1ac6af
Afdrada <Andres@AndresF>
2016-06-23 22:09:23 -0500

    esta es la conexion a la bd

---
+++ Softwareips/src/controlBD/Conexion.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlBD;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Andres
+ */
+public class Conexion {
+   private Connection conexion=null;
+   static ResultSet resultado, rst;
+   static Statement sentencia;
+   static String bd = "mbet";
+   static String Loggin= "root";
+   static String Pass="drada123456";
+   static String url="jdbc:postgresql://localhost:5432/postgres";
+ 
+   
+   public void conexionBD() throws SQLException{
+       try {
+           Class.forName("org.postgresql.Driver");
+           conexion = DriverManager.getConnection(url, Loggin, Pass);
+           if (conexion !=null){
+               System.out.println("conexion establecida con la bd"+ bd);
+           }
+       } catch (ClassNotFoundException ex) {
+           Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+       }
+   }
+   
+   
+  
+            
+}
